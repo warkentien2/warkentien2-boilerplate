0c342af0c8e129d5bdbaa4a2bc2eda7da5c7a6fb
Philip Warkentien II <philip.dw2@gmail.com>
2023-04-12 22:43:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React + Vite + TypeScript + Storybook + Theme Boilerplate
+
+## Running
+
+use node v16
+
+`$ yarn`
+`$ yarn dev`
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+function App(): ReactElement {
+  return (
+    <div className="App">Vite + React + TypeScript + Storybook + Theming</div>
+  );
+}
+
+export default App;
